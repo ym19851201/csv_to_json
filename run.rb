d919d4091aa51bf0eea2a01a23cf95e653ee1d42
miyazaki <miyazaki19851201@gmail.com>
2014-10-30 18:40:33 +0900

    fix bug

--- run.rb
+++ run.rb
@@ -8,12 +8,17 @@ file_path = STDIN.gets.chomp
 open(file_path) do |file|
   accounts = CSVParser.new(file.read).parse
   db = AccountDataBase.new(accounts)
-  puts "input attribute condition.\n ex) type \"age 30\" if you need data whose attribute \"age\" equal to \"30\".\n if you dont, just press Enter"
+  puts <<-EOS
+  input attribute condition.
+   ex) type \"age 30\" if you need data whose attribute \"age\" equal to \"30\".
+       if you dont, just press Enter"
+  EOS
   attr_condition = STDIN.gets.chomp.split " "
   if attr_condition.length <= 1
     data = db.all_accounts
   else
-    data = eval "db.accounts_#{attr_condition[0]}_equal_to(#{attr_condition[1]})"
+    data = eval "db.accounts_#{attr_condition[0]}_equal_to(\"#{attr_condition[1]}\")"
+    puts "db.accounts_#{attr_condition[0]}_equal_to(\"#{attr_condition[1]}\")"
   end
 
   puts 'input data bag id.'
